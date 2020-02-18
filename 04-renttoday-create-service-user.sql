95e034cee80423a3c2e2d062ebfad6e4c2dfa90a
Ondrej Valenta <ondrej.valenta@keenmate.com>
2020-02-18 02:36:29 +0100

    Update 04-renttoday-create-service-user.sql to by more concise

--- 04-renttoday-create-service-user.sql
+++ 04-renttoday-create-service-user.sql
@@ -3,9 +3,10 @@
 DROP OWNED BY renttoday; -- remove old permissions of the user
 DROP ROLE IF EXISTS renttoday; -- remove the user
 CREATE ROLE renttoday WITH LOGIN PASSWORD 'passwd'; -- create it again and assign proper permissions
+
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO renttoday;
+GRANT SELECT ON ALL TABLES IN SCHEMA const TO renttoday;
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA helpers TO renttoday;
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO renttoday;
 
 GRANT CONNECT ON DATABASE renttoday TO renttoday;
-
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO renttoday;
